f88a3dd35686ac33e3ea0b8f64b80210e822a605
trujilml <mickeytrj@gmail.com>
2021-12-07 21:05:47 -0600

    used hooks to hash passwords with bcrypt async and await function

--- models/User.js
+++ models/User.js
@@ -47,7 +47,18 @@ User.init(
     },
     {
         //TABLE CONFIGURATION OPTIONS GO HERE 
-        
+        hooks: {
+            //set up beforeCreate lifecycle "hook" functionality
+            async beforeCreate(newUserData) {
+                newUserData.password = await bcrypt.hash(newUserData.password, 10);
+                    return newUserData;
+            },
+            //set up beforeUpdate lifecycle "hook" functionality
+            async beforeUpdate(updatedUserData) {
+                updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+                return updatedUserData;
+            }
+        },
         //pass in our imported sequelize connection (the direct connection to our database)
         sequelize,
         //don't automatically create createdAt/updatedAt timestamp fields
