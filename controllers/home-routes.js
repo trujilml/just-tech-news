ba80afc7f19133873dd065250fd4eb1db7646e01
trujilml <mickeytrj@gmail.com>
2021-12-14 22:50:52 -0600

    cleaned up route files and connection file - check where the error is and clean more code when needed

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,49 +1,41 @@
-const sequelize = require('../config/connection');
-const { Post, User, Comment } = require('../models');
 const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { Post, User, Comment, Vote } = require('../models');
 
+// get all posts for homepage
 router.get('/', (req, res) => {
-    Post.findAll({
-        attributes: [
-            'id',
-            'post_url',
-            'title',
-            'created_at', 
-            [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
-        ],
-        include: [
-            {
-                model: Comment,
-                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
-                include: {
-                    model: User,
-                    attributes: ['username']
-                }
-            },
-            {
-                model: User,
-                attributes: ['username']
-            }
-        ]
-    })
+  console.log('======================');
+  Post.findAll({
+    attributes: [
+      'id',
+      'post_url',
+      'title',
+      'created_at',
+      [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+    ],
+    include: [
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        include: {
+          model: User,
+          attributes: ['username']
+        }
+      },
+      {
+        model: User,
+        attributes: ['username']
+      }
+    ]
+  })
     .then(dbPostData => {
-        //pass a single post object into the homepage template
-        const posts = dbPostData.map(post => post.get({ plain: true}));
-        res.render('homepage', {posts});
+      const posts = dbPostData.map(post => post.get({ plain: true }));
+
+      res.render('homepage', { posts });
     })
     .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-    // res.render('homepage', {
-    //     id: 1,
-    //     post_url: 'https://handlebarsjs.com/guide/',
-    //     title: 'Handlebars Docs',
-    //     created_at: new Date(),
-    //     vote_count: 10,
-    //     comments: [{}, {}],
-    //     user: {
-    //         username: 'test_user'
-    //     }
+      console.log(err);
+      res.status(500).json(err);
     });
 });
 
