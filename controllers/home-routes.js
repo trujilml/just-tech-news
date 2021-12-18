98842104d8ff67b77538a2aef489027fa3bb2b25
trujilml <mickeytrj@gmail.com>
2021-12-18 12:41:06 -0600

    rendered the login links to ensure users dont see logout appear if login appears - iykyk

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -31,8 +31,11 @@ router.get('/', (req, res) => {
     .then(dbPostData => {
       const posts = dbPostData.map(post => post.get({ plain: true }));
 
-      res.render('homepage', { posts });
-    })
+      res.render('homepage', {
+        posts,
+        loggedIn: req.session.loggedIn
+    });
+  })
     .catch(err => {
       console.log(err);
       res.status(500).json(err);
